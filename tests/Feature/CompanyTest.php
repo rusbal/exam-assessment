8f65b28e43d8d27d4e0c9641cfe3c0cc3700f8b9
Raymond Usbal <raymond@philippinedev.com>
2018-10-31 02:09:25 +0800

    jwt-auth properly installed

--- tests/Feature/CompanyTest.php
+++ tests/Feature/CompanyTest.php
@@ -5,14 +5,14 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class CompanyTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testCanCreate()
     {
         $response = $this->get('/');
 
--- tests/Unit/ModelTest.php
+++ tests/Unit/ModelTest.php
@@ -5,14 +5,14 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class ExampleTest extends TestCase
+class ModelTest extends TestCase
 {
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testCanCreateAndDeleteACompany()
     {
         $this->assertTrue(true);
     }
